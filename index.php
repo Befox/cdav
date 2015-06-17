77c31f14ef5b9b4c85bdcb77480ef27b14bd237a
Jean-Pierre Morfin <jpmorfin@befox.fr>
2015-06-17 16:52:33 +0200

    Sur la branche master: add webdav #*************************************************************** # Not in a bugXXX branch ! #***************************************************************

--- index.php
+++ index.php
@@ -67,7 +67,7 @@ use
 
 	// If your server is not on your webroot, make sure the following line has the
 	// correct information
-	$server->setBaseUri('/dolibarr/htdocs/cdav/');
+	$server->setBaseUri('/dolibarr/htdocs/cdav/index.php');
 
 	// The lock manager is reponsible for making sure users don't overwrite
 	// each others changes.
