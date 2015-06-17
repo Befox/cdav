8932c6af6d1296a3b8e6db69b5bd6113d4c1cc9f
Jean-Pierre Morfin <jpmorfin@befox.fr>
2015-06-17 16:50:20 +0200

    Sur la branche master: add webdav #*************************************************************** # Not in a bugXXX branch ! #***************************************************************

--- index.php
+++ index.php
@@ -51,6 +51,42 @@ $socid=0;
 if ($user->societe_id > 0) $socid = $user->societe_id;
 print_r($user);
 
+
+
+use
+    Sabre\DAV;
+
+	// The autoloader
+	require 'SabreDAV/vendor/autoload.php';
+
+	// Now we're creating a whole bunch of objects
+	$rootDirectory = new DAV\FS\Directory('SabreDAV/public');
+
+	// The server object is responsible for making sense out of the WebDAV protocol
+	$server = new DAV\Server($rootDirectory);
+
+	// If your server is not on your webroot, make sure the following line has the
+	// correct information
+	$server->setBaseUri('/dolibarr/htdocs/cdav/index.php');
+
+	// The lock manager is reponsible for making sure users don't overwrite
+	// each others changes.
+	$lockBackend = new DAV\Locks\Backend\File('SabreDAV/data/locks');
+	$lockPlugin = new DAV\Locks\Plugin($lockBackend);
+	$server->addPlugin($lockPlugin);
+
+	// This ensures that we get a pretty index in the browser, but it is
+	// optional. 
+	$server->addPlugin(new DAV\Browser\Plugin());
+
+	// All we need to do now, is to fire up the server
+	$server->exec();
+
+
+
+
+
+
 if (is_object($db)) $db->close();
 
 
