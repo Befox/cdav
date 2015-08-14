9d571503f4753979d053486c58b526c7667777f9
Jean-Pierre Morfin <jpmorfin@befox.fr>
2015-08-14 11:07:47 +0200

    Sur la branche master: rename fk_societe to fk_soc for dolibarr>=3.8 versions #*************************************************************** # Not in a bugXXX branch ! #***************************************************************

--- cdavurls.php
+++ cdavurls.php
@@ -83,12 +83,12 @@ elseif($type=='CalDAV')
 	if(isset($user->rights->agenda->allactions->read) && $user->rights->agenda->allactions->read)
 	{
 		if (versioncompare(versiondolibarrarray(), array(3,8,0)) >= 0)
-			$fk_soc_filedname = 'fk_soc';
+			$fk_soc_fieldname = 'fk_soc';
 		else
-			$fk_soc_filedname = 'fk_societe';
+			$fk_soc_fieldname = 'fk_societe';
 
 		$sql = 'SELECT u.rowid, u.login, u.firstname, u.lastname
-			FROM '.MAIN_DB_PREFIX.'user u WHERE '.$fk_soc_filedname.' IS NULL
+			FROM '.MAIN_DB_PREFIX.'user u WHERE '.$fk_soc_fieldname.' IS NULL
 			ORDER BY login';
 		$result = $db->query($sql);
 		while($row = $db->fetch_array($result))
