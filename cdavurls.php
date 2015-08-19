19021a3784812517ae15db7abcf5b6de47211685
Jean-Pierre Morfin <jpmorfin@befox.fr>
2015-08-19 14:47:12 +0200

    Sur la branche master: test version 3.8

--- cdavurls.php
+++ cdavurls.php
@@ -82,9 +82,9 @@ elseif($type=='CalDAV')
 	
 	if(isset($user->rights->agenda->allactions->read) && $user->rights->agenda->allactions->read)
 	{
-		if (versioncompare(versiondolibarrarray(), array(3,8,0))<0)
-			$fk_soc_fieldname = 'fk_societe';
-		else
+		//if (versioncompare(versiondolibarrarray(), array(3,8,0))<0)
+		//	$fk_soc_fieldname = 'fk_societe';
+		//else
 			$fk_soc_fieldname = 'fk_soc';
 
 		$sql = 'SELECT u.rowid, u.login, u.firstname, u.lastname
