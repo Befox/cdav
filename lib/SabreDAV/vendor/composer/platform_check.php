e1d5eb8b75355c0fe4add3a1086e459bc1fbf576
Eric Seigne <eric.seigne@cap-rel.fr>
2024-03-13 15:11:22 +0100

    update sabre lib

--- lib/SabreDAV/vendor/composer/platform_check.php
+++ lib/SabreDAV/vendor/composer/platform_check.php
@@ -4,10 +4,6 @@
 
 $issues = array();
 
-if (!(PHP_VERSION_ID >= 80000)) {
-    $issues[] = 'Your Composer dependencies require a PHP version ">= 8.0.0". You are running ' . PHP_VERSION . '.';
-}
-
 if ($issues) {
     if (!headers_sent()) {
         header('HTTP/1.1 500 Internal Server Error');
